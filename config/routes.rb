3359040f754eb751c1f5685ab83fc8c6ad2a09ba
wasaburou-matsuo <kurawasa.fire@gmail.com>
2026-06-26 09:57:11 +0900

    種目登録機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :exercises, only: [:new, :create]
   resources :gyms, only: [:new, :create] do
     resources :machines, only: [:index, :new, :create]
   end
