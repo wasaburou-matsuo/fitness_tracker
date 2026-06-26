f9c94c40f8e262818b48aa6e01c1b7f5bac1f491
wasaburou-matsuo <kurawasa.fire@gmail.com>
2026-06-26 09:28:41 +0900

    マシン選択画面を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :gyms, only: [:new, :create] do
-    resources :machines, only: [:new, :create]
+    resources :machines, only: [:index, :new, :create]
   end
   root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
