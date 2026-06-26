09b0d4b528029572289fd920b433f06d61a26b25
wasaburou-matsuo <kurawasa.fire@gmail.com>
2026-06-26 12:03:34 +0900

    トレーニング記録作成画面を追加

--- app/models/training_session.rb
+++ app/models/training_session.rb
@@ -3,6 +3,7 @@ class TrainingSession < ApplicationRecord
   belongs_to :exercise
   belongs_to :machine, optional: true
   has_many :training_sets, dependent: :destroy
+  accepts_nested_attributes_for :training_sets
 
   validates :trained_on, presence: true
   validates :exercise_id, uniqueness: { scope: [:user_id, :machine_id, :trained_on] }
